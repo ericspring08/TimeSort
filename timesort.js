ab41f94a464e8d3380ad41df17facfe5d56ff320
Eric Zhang <ericspring08@gmail.com>
2023-12-30 22:15:49 -0600

    format fixes

--- timesort.js
+++ timesort.js
@@ -1,8 +1,8 @@
 const {
-    Worker,
-    isMainThread,
-    parentPort,
-    workerData,
+  Worker,
+  isMainThread,
+  parentPort,
+  workerData,
 } = require("worker_threads");
 
 const threads = new Set();
@@ -10,24 +10,24 @@ const threads = new Set();
 let array = [2, 4, 1, 5, 2, 5, 6];
 
 function sortArray(array) {
-    if (!isMainThread) {
-        setTimeout(() => {
-            console.log(workerData);
-        }, 1000 * workerData);
-        parentPort.postMessage(workerData);
-    } else {
-        for (const element of array) {
-            threads.add(new Worker(__filename, {workerData: element}));
-        }
-        for (let worker of threads) {
-            worker.on("error", (err) => {
-                throw err;
-            });
-            worker.on("exit", () => {
-                threads.delete(worker);
-            });
-        }
+  if (!isMainThread) {
+    setTimeout(() => {
+      console.log(workerData);
+    }, 1000 * workerData);
+    parentPort.postMessage(workerData);
+  } else {
+    for (const element of array) {
+      threads.add(new Worker(__filename, { workerData: element }));
     }
+    for (let worker of threads) {
+      worker.on("error", (err) => {
+        throw err;
+      });
+      worker.on("exit", () => {
+        threads.delete(worker);
+      });
+    }
+  }
 }
 
 sortArray(array);
